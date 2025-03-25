846beafb12fa04b4fefe81771da4d7ab0dc1bc0d
ujeon <wj9304@gmail.com>
2025-03-25 17:34:19 +0900

    feat: 하위버전 OS 기존 사진 선택도구를 사용하도록 함
    - 기존 이미지 피커 관련 코드 복원
    - 기존 이미지 피커에서 동영상 파일도 선택할 수 있도록 개선
    - 기존 이미지 피커를 미디어 피커로 네이밍을 변경하고, 기타 코드 리팩토링
    
    This reverts commit e6a0ab75e162dfb3976a821f749d6d82f89ca2dc.

---
+++ library/src/main/java/com/classtinginc/image_picker/consts/Constants.java
@@ -0,0 +1,6 @@
+package com.classtinginc.image_picker.consts;
+
+public final class Constants {
+    public static final int DEFAULT_MAX_SIZE = 1;
+    public static final String DEFAULT_MEDIA_TYPE = "image";
+}
